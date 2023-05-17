64c0fd03ab0b9ed406fe222ee46bbadd84ab19d4
Jelle Wallenburg <jellewallenburg@gmail.com>
2023-05-17 14:39:39 +0200

    first push

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items= items;
+    this.length= length;
+  }
 
-  add(item) {}
-
+  add(item) {
+    for(let i=0; i < this.length; i++){
+      if(item <= this.items[i]){
+        this.items.splice(i, 0, item)
+      } else {
+        this.items.push(item);
+      }
+    }
+    this.length += 1
+  }
   get(pos) {}
 
   max() {}
@@ -15,3 +26,8 @@ class SortedList {
 }
 
 module.exports = SortedList;
+
+let test = new SortedList([1, 3], 2)
+console.log(test)
+test.add(4)
+console.log(test)
